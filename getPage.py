8149f3d9f87b10f28796bf76d81ca1d44f5aac31
Taylor Cowin <aliashaxxed@gmail.com>
2020-04-02 11:27:53 -0500

    new repo

--- getPage.py
+++ getPage.py
@@ -3,6 +3,7 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime
+import os
 
 #Get the file and feed it to BS4
 covidPage = requests.get('https://www.dallascounty.org/departments/dchhs/2019-novel-coronavirus.php')
@@ -10,18 +11,10 @@ covidPage = requests.get('https://www.dallascounty.org/departments/dchhs/2019-no
 soup = BeautifulSoup(covidPage.text, 'html.parser')
 
 allTables = soup.find_all("td")
-confirmedCases = allTables[6].text
-totalDead = allTables[7].text
+confirmedCases = allTables[5].text
+totalDead = allTables[6].text
 lengthWithoutAsterisk = len(confirmedCases) - 2;
 confirmedCases = confirmedCases[:lengthWithoutAsterisk]
-
-dateTimeVar = datetime.datetime.now()
-dateTimeFormatted = dateTimeVar.strftime("%x")
-
-
-
-
-f = open ("/var/www/html/CovidList.csv", "a")
-
-f.write(dateTimeFormatted + "," + confirmedCases + "," + totalDead + "\n")
-f.close()
+print (allTables)
+print (confirmedCases)
+print (totalDead)
